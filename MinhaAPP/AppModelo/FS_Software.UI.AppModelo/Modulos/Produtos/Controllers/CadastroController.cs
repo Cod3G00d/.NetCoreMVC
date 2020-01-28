9f87e86151797b451ed1a64aa2fc458755207ef2
felipe.souza <Dell@0178>
2020-01-28 15:06:38 -0300

    Alterções Curso

---
+++ MinhaAPP/AppModelo/FS_Software.UI.AppModelo/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FS_Software.UI.AppModelo.Areas.Produtos.Controllers
+{  
+
+    [Area(nameof(Produtos))]  // CRM is the another-folder name inside Areas folder
+    [Route("Produtos/[controller]/[action]")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
